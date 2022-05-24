798ee41655f23b7d3d28b795e32e57bd58416cad
dvp <dmitri_portnov@yahoo.com>
2022-05-24 10:17:55 +0300

    tests: more tests for npz2vtk

--- tests/cli/test_npz2vtk.py
+++ tests/cli/test_npz2vtk.py
@@ -1,3 +1,5 @@
+"""Tests for npz2vtk CLI module."""
+
 import shutil
 
 from pathlib import Path
@@ -5,11 +7,11 @@ from pathlib import Path
 import pytest
 
 from mckit_meshes.cli.runner import mckit_meshes
-from mckit_meshes.fmesh import FMesh
 
-# @pytest.fixture()
-# def source(data):
-#     return data / "1.m"
+
+@pytest.fixture()
+def source(data):
+    return data / "1004.npz"
 
 
 def test_help(runner):
@@ -20,11 +22,11 @@ def test_help(runner):
     assert "Usage: " in result.output
 
 
-def test_with_prefix(tmp_path, runner, data):
+def test_with_prefix(tmp_path, runner, data, source):
     prefix = tmp_path
     result = runner.invoke(
         mckit_meshes,
-        args=["npz2vtk", "--prefix", prefix, str(data / "1004.npz")],
+        args=["npz2vtk", "--prefix", prefix, str(source)],
         catch_exceptions=False,
     )
     assert result.exit_code == 0
@@ -52,85 +54,30 @@ def test_multiple_files(tmp_path, runner, data):
         ).exists(), """When multiple npz files are specified the vtk files should be created for every one."""
 
 
-# def test_without_prefix(cd_tmpdir, runner, source):
-#     result = runner.invoke(
-#         mckit_meshes, args=["npz2vtk", str(source)], catch_exceptions=False
-#     )
-#     assert result.exit_code == 0
-#     cwd = Path.cwd()
-#     output_path = cwd / "npz" / "1004.npz"
-#     assert output_path.exists()
-#
-#
-# def test_existing_mesh_tally_file_and_not_specified_mesh_tally(
-#     cd_tmpdir, runner, source
-# ):
-#     t = Path.cwd()
-#     shutil.copy(source, t)
-#     input_path = t / "1.m"
-#     assert input_path.exists()
-#     output_path = t / "npz/1004.npz"
-#     assert not output_path.exists()
-#     result = runner.invoke(mckit_meshes, args=["npz2vtk"], catch_exceptions=False)
-#     assert result.exit_code == 0
-#     assert (
-#         output_path.exists()
-#     ), "Failed to process meshtal file in current directory with empty command line"
-#
-#
-# def test_no_mesh_tally_file_and_not_specified_mesh_tally(cd_tmpdir, runner):
-#     assert not list(
-#         f for f in Path.cwd().glob("*.m")
-#     ), "There shouldn't be any .m files in current directory"
-#     result = runner.invoke(mckit_meshes, args=["npz2vtk"], catch_exceptions=False)
-#     assert result.exit_code == 0, "Should be noop, when nothing to do"
-#     assert (
-#         "WARNING" in result.output and "nothing to do" in result.output
-#     ), "Should warn, when nothing to do"
-#
-#
-# def test_not_existing_mesh_tally_file(runner):
-#     result = runner.invoke(
-#         mckit_meshes, args=["npz2vtk", "not-existing.m"], catch_exceptions=False
-#     )
-#     assert result.exit_code > 0
-#     assert "does not exist" in result.output
-#
-#
-# def test_failure_on_existing_output_file_when_override_is_not_set(
-#     tmp_path, runner, source
-# ):
-#     prefix = tmp_path / "npz"
-#     output_path = prefix / "1004.npz"
-#     output_path.parent.mkdir(parents=True, exist_ok=True)
-#     output_path.touch(exist_ok=True)
-#     assert output_path.exists()
-#     result = runner.invoke(
-#         mckit_meshes,
-#         args=["npz2vtk", "-p", prefix, str(source)],
-#         catch_exceptions=True,
-#     )
-#     assert result.exit_code != 0
-#     errmsg = f"""\
-# Cannot override existing file \"{output_path}\".
-# Please remove the file or specify --override option"""
-#     assert errmsg in result.output
-#
-#
-# def test_long_mesh_number(cd_tmpdir, runner, data):
-#     """Check if mesh number representation in npz file is long enough to handle large numbers."""
-#     prefix = Path.cwd()
-#     _input = data / "2035224.m"
-#     result = runner.invoke(
-#         mckit_meshes,
-#         args=["npz2vtk", "-p", prefix, str(_input)],
-#         catch_exceptions=False,
-#     )
-#     assert result.exit_code == 0, "should successfully process {}".format(_input)
-#     prefix = Path(prefix)
-#     npz_path = prefix / "2035224.npz"
-#     assert npz_path.exists(), "should create {}".format(npz_path)
-#     mesh = FMesh.load_npz(npz_path)
-#     assert (
-#         mesh.name == 2035224
-#     ), "Should correctly save and load the 2035224 mesh id, which requires 32 bit"
+def test_without_prefix(cd_tmpdir, runner, source):
+    result = runner.invoke(
+        mckit_meshes, args=["npz2vtk", str(source)], catch_exceptions=False
+    )
+    assert result.exit_code == 0
+    cwd = Path.cwd()
+    output_path = cwd / "1004.vtr"
+    assert output_path.exists()
+
+
+def test_no_npz_files_and_not_specified_npz(cd_tmpdir, runner):
+    assert not list(
+        f for f in Path.cwd().glob("*.npz")
+    ), "There shouldn't be any .m files in current directory"
+    result = runner.invoke(mckit_meshes, args=["npz2vtk"], catch_exceptions=False)
+    assert result.exit_code == 0, "Should be noop, when nothing to do"
+    assert (
+        "WARNING" in result.output and "nothing to do" in result.output
+    ), "Should warn, when nothing to do"
+
+
+def test_not_existing_input_file(runner):
+    result = runner.invoke(
+        mckit_meshes, args=["npz2vtk", "not-existing.npz"], catch_exceptions=False
+    )
+    assert result.exit_code > 0
+    assert "does not exist" in result.output
