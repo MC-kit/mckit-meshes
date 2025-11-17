58f28413e298ca9cb017bf044b41c2e954473843
dvp <d.portnov@iterrf.ru>
2025-11-17 16:30:38 +0300

    feat[notebook]: plotting mesh pictures n,p fluxes

--- notebooks/plotting/25-11-13-dvp-plot-meshes-on-ps.py
+++ notebooks/plotting/25-11-13-dvp-plot-meshes-on-ps.py
@@ -30,9 +30,13 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import colors, colormaps as cm
+from matplotlib import ticker
 
 # %%
 # %matplotlib inline
+
+# %% [markdown]
+# See matplotlib [styles](https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html)
 
 # %%
 # plt.style.available
@@ -51,10 +55,10 @@ FIG_WIDTH = int(
 )  # Optimal for full page witdh graphs, for 1 column graph in 2 column publications use 8cm.
 FIG_HEIGHT = FIG_WIDTH
 plt.rcParams["figure.figsize"] = (FIG_WIDTH, FIG_HEIGHT)
-plt.style.use("fivethirtyeight")
-plt.rc("grid", color="gray", linestyle="solid")
-plt.rc("xtick", direction="out", color="gray")
-plt.rc("ytick", direction="out", color="gray")
+plt.style.use("petroff10")
+# plt.rc("grid", color="gray", linestyle="solid")
+# plt.rc("xtick", direction="out", color="gray")
+# plt.rc("ytick", direction="out", color="gray")
 
 # %%
 from mckit_meshes.plot import load_plotm_file, Page, plot_ps_page, BriefTicksAroundOneTicker, plot_2d_distribution
@@ -112,7 +116,7 @@ neutron_flux_mesh = FMesh.load_npz(NPZ_DIR / "1214.npz")
 # %%
 neutron_flux_mesh.totals.shape
 
-# %% jupyter={"outputs_hidden": true}
+# %%
 eq_mid_height_idx = neutron_flux_mesh.kbins.searchsorted(50)
 0.5*(neutron_flux_mesh.kbins[eq_mid_height_idx-1] + neutron_flux_mesh.kbins[eq_mid_height_idx])
 
@@ -126,12 +130,22 @@ X, Y = np.meshgrid(x, y)
 
 
 # %%
+def mids(x):
+    return 0.5*(x[1:] + x[:-1])
+
+
+# %%
+set(cm.keys())
+
+
+# %%
 def plot_2d_distribution(x, y, data, fig, ax,
     *,
     color_bar_title=r"$\frac{1} {cm^{2} \cdot s}$",
     max_log_power=None,
     min_max_log_ratio=1e-4,
     transform=None,
+    levels=None,
 ):
     if max_log_power is None:
         max_log_power = int(np.log10(data.max()))
@@ -153,10 +167,35 @@ def plot_2d_distribution(x, y, data, fig, ax,
         # transform=transform,
     )
     color_bar = fig.colorbar(pcm, ax=ax, shrink=0.8)
-    color_bar.ax.set_title(color_bar_title, fontsize=8)
+    color_bar.ax.set_title(color_bar_title, fontsize=12)
     tick_formatter = BriefTicksAroundOneTicker()
     color_bar.ax.yaxis.set_major_formatter(tick_formatter)
     color_bar.outline.set_edgecolor("white")
+    if levels is not None:
+        _colors= "k" # cm.get("Wistia_r")(norm(np.array(levels)))
+        contours = ax.contour(
+            mids(x),
+            mids(y),
+            data,
+            norm=norm,
+            levels=levels,
+            colors=_colors,
+            linewidths=1.0,
+            # alpha=0.5,
+        )
+        levels = contours.levels
+        contour_labeled_levels = levels
+        fmt = ticker.LogFormatterMathtext()
+        fmt.set_axis(ax)
+        ax.clabel(
+            contours,
+            contour_labeled_levels,
+            inline=1.0,
+            # fmt="%.1g",
+            fmt=fmt,
+            # colors="k",
+            fontsize=9,
+        )
     
 
 
@@ -168,7 +207,7 @@ axes.set_aspect("equal")
 axes.set_xlim(x[0], x[-1])
 axes.set_ylim(y[0], y[-1])
 plot_ps_page(axes, p)
-plot_2d_distribution(x, y, data, fig, axes)
+plot_2d_distribution(x, y, data, fig, axes, levels = np.pow(10, np.array([8,9,10,11])))
 plt.savefig((NPZ_DIR / "total-neutron-flux-pz=50").with_suffix(".png"), dpi=1200)
 plt.show()
 
@@ -186,8 +225,8 @@ axes.set_aspect("equal")
 axes.set_xlim(x[0], x[-1])
 axes.set_ylim(y[0], y[-1])
 plot_ps_page(axes, p)
-plot_2d_distribution(x, y, photon_data, fig, axes)
-plt.savefig((NPZ_DIR / "total-photon-flux-pz=50").with_suffix(".png"), dpi=1200)
+plot_2d_distribution(x, y, photon_data, fig, axes, levels = np.pow(10, np.array([7, 8, 9, 10])))
+plt.savefig((NPZ_DIR / "total-photon-flux-pz=50").with_suffix(".png"), dpi=1200, bbox_inches="tight", transparent=True, metadata={"Title": "Total photon flux at PZ=50"})
 plt.show()
 
 # %%
